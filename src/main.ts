c9b07d239648973f7de6589f1b69d0c12222ef37
haungxi <1842196429@qq.com>
2024-01-22 11:23:01 +0800

    ✨ feat(格式化): 格式化返回结果

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { FormatResponseInterceptor } from './interceptor/format.response'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
